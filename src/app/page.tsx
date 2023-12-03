64b4461bb49b919a247db697ff681da0c3afa98e
jeremyda173 <jh4017034@gmail.com>
2023-12-03 13:13:00 -0400

    feat: change page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,121 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
-import React, { useState, useEffect } from 'react';
-import 'tailwindcss/tailwind.css';
-import MessiLogo from '@/../../public/messi-logo.png';  // Asegúrate de importar la ruta correcta de la imagen
+import React, { useState } from 'react';
+import MessiLogo from '@/../../public/messi-logo.png';
 import Image from 'next/image';
+import 'tailwindcss/tailwind.css';
+import Portada from '@/../../public/Portada.jpg';
 
-function Home() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [equiposOpen, setEquiposOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-    setEquiposOpen(false);
-  };
-
-  const toggleEquipos = () => {
-    setEquiposOpen(!equiposOpen);
-  };
-
-  useEffect(() => {
-    console.log("use client");
-  }, []);
+const Dashboard = () => {
+  const [activeOption, setActiveOption] = useState(null);
 
   return (
-    <div className="bg-gradient-to-b from-gray-900 to-gray-800 text-black min-h-screen p-4">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+    <div className="min-h-screen flex flex-col relative overflow-hidden">
+      {/* Fondo ondulado blanco encima de la imagen */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        className="absolute top-0 left-0 w-full z-0"
+        style={{ zIndex: -1 }}
+      >
+        <path fill="#ffffff" fillOpacity="1" d="M0,288L1440,96L1440,320L0,320Z"></path>
+      </svg>
 
-        <div className="md:h-16 md:mb-4 flex items-center mt-8 order-2 md:order-1"> 
-          <button
-            className="ml-4 px-2 py-1 rounded text-white transition-colors"
-            onClick={toggleMenu}
-          >
-            Menú
-          </button>
+      {/* Fondo ondulado debajo de la imagen */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        className="absolute bottom-0 left-0 w-full z-0"
+      >
+        <path fill="#0099ff" fillOpacity="1" d="M0,288L1440,96L1440,320L0,320Z"></path>
+      </svg>
 
-          {menuOpen && (
-            <div className="absolute top-8 right-20 w-64 h-full p-4 rounded shadow-md" style={{ background: 'linear-gradient(to bottom, #004d98, rgba(0, 77, 152, 0))' }}>
-              <div className="flex justify-between items-center mb-4">
-                <div className="text-xl font-bold text-white">Menú</div>
-                <button
-                  className="text-white hover:text-white focus:outline-none"
-                  onClick={toggleMenu}
-                >
-                  <div className='hover:bg-red-600 p-2'>  
-                    X
-                  </div> 
-                </button>
-              </div>
-              <ul className="space-y-2">
-                <li><a href="#" className="hover:text-purple-400">Historial de partidos</a></li>
-                <li className="mb-2">
-                  <div className="flex items-center">
-                    <button
-                      className="text-left focus:outline-none"
-                      onClick={toggleEquipos}
-                    >
-                      Equipos
-                    </button>
-                  </div>
-                  {equiposOpen && (
-                    <ul className="pl-4 space-y-1">
-                      <li><a href="#" className="hover:text-purple-400 pb-2 pt-2">Argentina</a></li>
-                      <li><a href="#" className="hover:text-purple-400 pb-2 pt-2">Paris Saint-Germain</a></li>
-                      <li><a href="#" className="hover:text-purple-400 pb-2 pt-2">Miami</a></li>
-                      <li><a href="#" className="hover:text-purple-400 pb-2 pt-2">Barcelona</a></li>
-                    </ul>
-                  )}
-                </li>
-                <li><a href="#" className="hover:text-purple-400">Vida personal</a></li>
-                <li><a href="#" className="hover:text-purple-400">Historia</a></li>
-                <li><a href="#" className="hover:text-purple-400">Estadísticas y premios</a></li>
-              </ul>
-            </div>
-          )}
-        </div>
-
-        <div className="md:h-16 md:mb-4 md:ml-auto order-1 md:order-2"> 
-          <Image src={MessiLogo} alt="Messi Logo" width={50} height={50} />
-        </div>
-
-        <div className="text-center mt-4 md:mt-0 order-3">
-          {/* Contenido de la Página */}
-          <div className="text-black">
-            {/* Contenido de la sección de Equipos */}
-            {equiposOpen && (
-              <div className="pl-4 space-y-1">
-                <p>Contenido de la sección de Equipos...</p>
-              </div>
-            )}
+      {/* Contenido del Dashboard */}
+      <div className="flex-1 relative z-10">
+        {/* Menú Horizontal */}
+        <div className="bg-gradient-to-r from-green-400 to-blue-500 p-4 flex items-center justify-between">
+          {/* Logo de Messi */}
+          <div className="flex items-center">
+            <Image src={MessiLogo} alt="Messi Logo" width={50} height={50} />
           </div>
+
+          {/* Opciones del Menú */}
+          <div className="flex items-center">
+            <a
+              href="#"
+              className={`mr-4 hover:text-gray-300 ${activeOption === 'home' ? 'border-b-2 border-white' : ''}`}
+              onMouseEnter={() => setActiveOption('home')}
+              onMouseLeave={() => setActiveOption(null)}
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className={`mr-4 hover:text-gray-300 ${activeOption === 'matchHistory' ? 'border-b-2 border-white' : ''}`}
+              onMouseEnter={() => setActiveOption('matchHistory')}
+              onMouseLeave={() => setActiveOption(null)}
+            >
+              Match History
+            </a>
+            <a
+              href="#"
+              className={`mr-4 hover:text-gray-300 ${activeOption === 'awards' ? 'border-b-2 border-white' : ''}`}
+              onMouseEnter={() => setActiveOption('awards')}
+              onMouseLeave={() => setActiveOption(null)}
+            >
+              Awards
+            </a>
+            <a
+              href="#"
+              className={`mr-4 hover:text-gray-300 ${activeOption === 'teams' ? 'border-b-2 border-white' : ''}`}
+              onMouseEnter={() => setActiveOption('teams')}
+              onMouseLeave={() => setActiveOption(null)}
+            >
+              Teams
+            </a>
+            <a
+              href="#"
+              className={`mr-4 hover:text-gray-300 ${activeOption === 'socialMedia' ? 'border-b-2 border-white' : ''}`}
+              onMouseEnter={() => setActiveOption('socialMedia')}
+              onMouseLeave={() => setActiveOption(null)}
+            >
+              Social Media
+            </a>
+            <a
+              href="#"
+              className={`mr-4 hover:text-gray-300 ${activeOption === 'family' ? 'border-b-2 border-white' : ''}`}
+              onMouseEnter={() => setActiveOption('family')}
+              onMouseLeave={() => setActiveOption(null)}
+            >
+              Family
+            </a>
+    
+            {/* Agrega más opciones según sea necesario */}
+          </div>
+        </div>
+
+        {/* Imagen de portada */}
+        <Image src={Portada} alt="Portada" className="w-full h-auto" />
+
+        {/* Contenido específico del dashboard */}
+        <div className="container mx-auto p-8">
+          <p className="text-2xl font-bold mb-4">Welcome to your Dashboard</p>
+
+          {/* Sección de la historia personal de Messi */}
+          <div className="mb-8">
+            <h2 className="text-xl font-bold mb-2">Messi's Personal History</h2>
+            <p>
+              Lionel Messi, born on June 24, 1987, in Rosario, Argentina, is considered one of the greatest football players of all time.
+              {/* Agrega más detalles de la historia personal de Messi según sea necesario */}
+            </p>
+          </div>
+
+          {/* Otro contenido del dashboard */}
+          <p>Dashboard content...</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Home;
+export default Dashboard;
