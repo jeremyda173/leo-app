a88efc5f50d8f90fb75abda6697e4c4d05b01ac1
jeremyda173 <jh4017034@gmail.com>
2023-12-05 18:19:00 -0400

    rename index.tsx to page.tsx

---
+++ src/app/family/page.tsx
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css';
+import React from 'react'
+
+function Family(){
+    return(
+        <div className="container mx-auto px-4 sm:px-8">Family</div>
+    );
+}
+export default Family;
