1a3da60d1f9a75378152993681dc2c163fa9d9ee
jeremyda173 <jh4017034@gmail.com>
2023-12-05 17:29:52 -0400

    feat: modify page.tsx for home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,95 +11,94 @@ const Dashboard = () => {
 
   return (
     <div className="min-h-screen flex flex-col relative overflow-hidden">
+      {/* Menú Horizontal */}
+      <div className="bg-gradient-to-r from-green-400 to-blue-500 p-4 flex items-center justify-between">
+        {/* Logo de Messi */}
+        <div className="flex items-center">
+          <Image src={MessiLogo} alt="Messi Logo" width={50} height={50} />
+        </div>
+
+        {/* Opciones del Menú */}
+        <div className="flex items-center">
+          <a
+            href="#"
+            className={`mr-4 hover:text-gray-300 ${activeOption === 'home' ? 'border-b-2 border-white' : ''}`}
+            onMouseEnter={() => setActiveOption('home')}
+            onMouseLeave={() => setActiveOption(null)}
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className={`mr-4 hover:text-gray-300 ${activeOption === 'matchHistory' ? 'border-b-2 border-white' : ''}`}
+            onMouseEnter={() => setActiveOption('matchHistory')}
+            onMouseLeave={() => setActiveOption(null)}
+          >
+            Match History
+          </a>
+          <a
+            href="#"
+            className={`mr-4 hover:text-gray-300 ${activeOption === 'awards' ? 'border-b-2 border-white' : ''}`}
+            onMouseEnter={() => setActiveOption('awards')}
+            onMouseLeave={() => setActiveOption(null)}
+          >
+            Awards
+          </a>
+          <a
+            href="/dashboard"
+            className={`mr-4 hover:text-gray-300 ${activeOption === 'teams' ? 'border-b-2 border-white' : ''}`}
+            onMouseEnter={() => setActiveOption('teams')}
+            onMouseLeave={() => setActiveOption(null)}
+          >
+            Teams
+          </a>
+          <a
+            href="#"
+            className={`mr-4 hover:text-gray-300 ${activeOption === 'socialMedia' ? 'border-b-2 border-white' : ''}`}
+            onMouseEnter={() => setActiveOption('socialMedia')}
+            onMouseLeave={() => setActiveOption(null)}
+          >
+            Social Media
+          </a>
+          <a
+            href="#"
+            className={`mr-4 hover:text-gray-300 ${activeOption === 'family' ? 'border-b-2 border-white' : ''}`}
+            onMouseEnter={() => setActiveOption('family')}
+            onMouseLeave={() => setActiveOption(null)}
+          >
+            Family
+          </a>
+          {/* Agrega más opciones según sea necesario */}
+        </div>
+      </div>
+
       {/* Contenido del Dashboard */}
-      <div className="flex-1 relative z-10">
-        {/* Menú Horizontal */}
-        <div className="bg-gradient-to-r from-green-400 to-blue-500 p-4 flex items-center justify-between">
-          {/* Logo de Messi */}
-          <div className="flex items-center">
-            <Image src={MessiLogo} alt="Messi Logo" width={50} height={50} />
-          </div>
-
-          {/* Opciones del Menú */}
-          <div className="flex items-center">
-            <a
-              href="#"
-              className={`mr-4 hover:text-gray-300 ${activeOption === 'home' ? 'border-b-2 border-white' : ''}`}
-              onMouseEnter={() => setActiveOption('home')}
-              onMouseLeave={() => setActiveOption(null)}
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className={`mr-4 hover:text-gray-300 ${activeOption === 'matchHistory' ? 'border-b-2 border-white' : ''}`}
-              onMouseEnter={() => setActiveOption('matchHistory')}
-              onMouseLeave={() => setActiveOption(null)}
-            >
-              Match History
-            </a>
-            <a
-              href="#"
-              className={`mr-4 hover:text-gray-300 ${activeOption === 'awards' ? 'border-b-2 border-white' : ''}`}
-              onMouseEnter={() => setActiveOption('awards')}
-              onMouseLeave={() => setActiveOption(null)}
-            >
-              Awards
-            </a>
-            <a
-              href="#"
-              className={`mr-4 hover:text-gray-300 ${activeOption === 'teams' ? 'border-b-2 border-white' : ''}`}
-              onMouseEnter={() => setActiveOption('teams')}
-              onMouseLeave={() => setActiveOption(null)}
-            >
-              Teams
-            </a>
-            <a
-              href="#"
-              className={`mr-4 hover:text-gray-300 ${activeOption === 'socialMedia' ? 'border-b-2 border-white' : ''}`}
-              onMouseEnter={() => setActiveOption('socialMedia')}
-              onMouseLeave={() => setActiveOption(null)}
-            >
-              Social Media
-            </a>
-            <a
-              href="#"
-              className={`mr-4 hover:text-gray-300 ${activeOption === 'family' ? 'border-b-2 border-white' : ''}`}
-              onMouseEnter={() => setActiveOption('family')}
-              onMouseLeave={() => setActiveOption(null)}
-            >
-              Family
-            </a>
-            {/* Agrega más opciones según sea necesario */}
-          </div>
+      <div className="flex-1 relative z-10 container mx-auto p-8 flex items-center">
+        {/* Imagen más grande */}
+        <div className='w-3/4 border rounded overflow-hidden mr-8'>
+          <Image src={Portada} alt={'Messi'} />
         </div>
 
-        {/* Imagen de portada centrada con bordes de gradiente en las esquinas */}
-        <div className="w-full h-auto overflow-hidden mt-16 relative">
-          <div className="relative">
-            <div className="absolute top-0 left-0 w-6 h-6 bg-gradient-to-tr from-blue-500 to-white"></div>
-            <div className="absolute top-0 right-0 w-6 h-6 bg-gradient-to-tl from-red-500 to-white"></div>
-            <div className="absolute bottom-0 left-0 w-6 h-6 bg-gradient-to-br from-red-500 to-white"></div>
-            <div className="absolute bottom-0 right-0 w-6 h-6 bg-gradient-to-bl from-blue-500 to-white"></div>
-            <Image src={Portada} alt="Portada" width={400} height={600} />
-          </div>
-        </div>
-
-        {/* Contenido específico del dashboard */}
-        <div className="container mx-auto p-8">
-          <p className="text-2xl font-bold mb-4">Welcome to your Dashboard</p>
+        {/* Texto más pequeño */}
+        <div className="flex flex-col max-w-md">
+          <p className="text-lg font-bold mb-4">Welcome to page Leo Messi</p>
 
           {/* Sección de la historia personal de Messi */}
           <div className="mb-8">
             <h2 className="text-xl font-bold mb-2">Messi's Personal History</h2>
-            <p>
-              Lionel Messi, born on June 24, 1987, in Rosario, Argentina, is considered one of the greatest football players of all time.
-              {/* Agrega más detalles de la historia personal de Messi según sea necesario */}
+            <p className='text-justify'>
+              Lionel Messi es un futbolista argentino nacido el 24 de junio de 1987 en Rosario, Argentina. Desde joven, Messi mostró un talento excepcional para el fútbol.
             </p>
+                <br></br>
+            <p className='text-justify'>
+              A la edad de 13 años, se trasladó a Barcelona, España, para unirse a la academia juvenil del FC Barcelona, La Masia. Messi rápidamente se destacó en las categorías juveniles del Barcelona, 
+              y a los 16 años hizo su debut con el primer equipo en un partido amistoso. Su actuación impresionante llevó al entrenador Frank Rijkaard a darle la oportunidad en la liga española.
+            </p>
+            {/* Agrega más detalles de la historia personal de Messi según sea necesario */}
           </div>
 
           {/* Otro contenido del dashboard */}
-          <p>Dashboard content...</p>
+          <p>Contenido del dashboard...</p>
         </div>
       </div>
     </div>
