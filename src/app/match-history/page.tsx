6269036259e9ed2903c245c1878541d84c79b7af
jeremyda173 <jh4017034@gmail.com>
2023-12-05 18:13:59 -0400

    rename index.tsx to page.tsx

---
+++ src/app/match-history/page.tsx
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css';
+import React from 'react'
+
+function MatchHistory(){
+    return(
+        <div className="container mx-auto px-4 sm:px-8">Match History</div>
+    );
+}
+export default MatchHistory;
