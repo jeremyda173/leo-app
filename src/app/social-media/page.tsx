3de6499b6b9adcf91f5741d1d1054ac48c094ed1
jeremyda173 <jh4017034@gmail.com>
2023-12-05 18:12:56 -0400

    feat: rename index.tsx to page.tsx

---
+++ src/app/social-media/page.tsx
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css';
+import React from 'react'
+
+function SocialMedia(){
+    return(
+        <div className="container mx-auto px-4 sm:px-8">Social Media</div>
+    );
+}
+export default SocialMedia;
