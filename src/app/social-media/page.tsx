61d2a72a7c3674edd95e38b370a7147e3e8a2b90
jeremyda173 <jh4017034@gmail.com>
2023-12-07 23:01:47 -0400

    delete page.tsx from social media

--- src/app/social-media/page.tsx
+++
@@ -1,9 +0,0 @@
-import 'tailwindcss/tailwind.css';
-import React from 'react'
-
-function SocialMedia(){
-    return(
-        <div className="container mx-auto px-4 sm:px-8">Social Media</div>
-    );
-}
-export default SocialMedia;
