8bb25e80fc1cb4d3dff277fb9f4273644a7680e2
jeremyda173 <jh4017034@gmail.com>
2024-01-07 22:17:08 -0400

    feat: add new router

--- src/app/awards/individuals/page.tsx
+++ src/app/awards/individuals/page.tsx
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css';
 import BallonDOr from '@/app/awards/individuals/balon-oro/page';
 import Botaoro from '@/app/awards/individuals/bota-oro/page';
 import GoldenBoy from '@/app/awards/individuals/golden-boy/page';
+import TheBest from '@/app/awards/individuals/the-best/page';
 import React from 'react';
 
 function PresentationIn() {
@@ -10,6 +11,7 @@ function PresentationIn() {
         < BallonDOr />
         < Botaoro />
         < GoldenBoy />
+        < TheBest />
     </div>
   );
 }
