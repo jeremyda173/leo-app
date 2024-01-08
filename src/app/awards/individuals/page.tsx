f6fe314ce3381fbfcf6b91acb5939a00157aac6d
jeremyda173 <jh4017034@gmail.com>
2024-01-07 22:41:54 -0400

    feat: edit h1

--- src/app/awards/individuals/page.tsx
+++ src/app/awards/individuals/page.tsx
@@ -1,17 +1,26 @@
 import 'tailwindcss/tailwind.css';
+import React from 'react';
+
 import BallonDOr from '@/app/awards/individuals/balon-oro/page';
 import Botaoro from '@/app/awards/individuals/bota-oro/page';
 import GoldenBoy from '@/app/awards/individuals/golden-boy/page';
 import TheBest from '@/app/awards/individuals/the-best/page';
-import React from 'react';
 
 function PresentationIn() {
   return (
-    <div>
-        < BallonDOr />
-        < Botaoro />
-        < GoldenBoy />
-        < TheBest />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+      <div className="col-span-1">
+        <BallonDOr />
+      </div>
+      <div className="col-span-1">
+        <Botaoro />
+      </div>
+      <div className="col-span-1">
+        <GoldenBoy />
+      </div>
+      <div className="col-span-1">
+        <TheBest />
+      </div>
     </div>
   );
 }
