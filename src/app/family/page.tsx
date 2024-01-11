0d496eb9c3ceadd4cd8b4796899db8b4562f8a92
jeremyda173 <jh4017034@gmail.com>
2024-01-10 23:08:52 -0400

    feat: Using routes for the work

--- src/app/family/page.tsx
+++ src/app/family/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 import Antonella from '@/app/family/carta1';
+import Ciro from '@/app/family/carta2';
+import Thiago from '@/app/family/carta4'
 
 function Family() {
     return (
         <div className="container mx-auto px-4 sm:px-8 mt-8">
-            < Antonella />
+            <Antonella />
+            <Ciro />
+            <Thiago />
         </div>
     );
 }
