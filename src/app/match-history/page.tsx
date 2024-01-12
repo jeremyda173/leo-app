889c8a9afb4bec3a406bbacdebe025a5cb04848e
jeremyda173 <jh4017034@gmail.com>
2024-01-11 21:02:09 -0400

    feat: use the new card component

--- src/app/match-history/page.tsx
+++ src/app/match-history/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MatchHistory from "../match-history/carta";
+import MatchHistoryB from "../match-history/carta2";
+import NextGamefrom from '../match-history/next-game';
 
 function CartaMatch() {
  return (
     <div className="flex space-x-4">
     <MatchHistory />
-    <MatchHistory />
-    <MatchHistory />
+    <MatchHistoryB />
+    <NextGamefrom />
     </div>
  );
 }
