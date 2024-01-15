6c6916bbeaba18c6579bf163434cd4c6946ae712
jeremyda173 <jh4017034@gmail.com>
2024-01-14 21:01:04 -0400

    feat: modify page.tsx for add new routes and desing

--- src/app/match-history/page.tsx
+++ src/app/match-history/page.tsx
@@ -1,17 +1,50 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import MatchHistory from "../match-history/carta";
 import MatchHistoryB from "../match-history/carta2";
-import NextGamefrom from '../match-history/next-game';
+import MatchHistoryC from "../match-history/carta3";
+import NextGameform from '../match-history/next-game';
 
 function CartaMatch() {
- return (
-    <div className="flex space-x-4">
-    <MatchHistory />
-    <MatchHistoryB />
-    <NextGamefrom />
-    </div>
- );
-}
+  const cards = ["MatchHistory", "MatchHistoryB", "MatchHistoryC", "NextGameform"];
+  const [activeCardIndex, setActiveCardIndex] = useState(0);
 
+  const handleNextClick = () => {
+    const newIndex = (activeCardIndex + 1) % cards.length;
+    setActiveCardIndex(newIndex);
+  };
+
+  const handlePrevClick = () => {
+    const newIndex = (activeCardIndex - 1 + cards.length) % cards.length;
+    setActiveCardIndex(newIndex);
+  };
+
+  const activeCard = cards[activeCardIndex];
+
+  return (
+    <div>
+      <div className="flex space-x-4">
+        {activeCard === "MatchHistory" && <MatchHistory />}
+        {activeCard === "MatchHistoryB" && <MatchHistoryB />}
+        {activeCard === "MatchHistoryC" && <MatchHistoryC />}
+        {activeCard === "NextGameform" && <NextGameform />}
+      </div>
+      <div className="flex justify-center mt-4">
+        <button
+          className="bg-blue-500 text-white p-2 rounded-full mr-4"
+          onClick={handlePrevClick}
+        >
+          &larr; Anterior
+        </button>
+        <button
+          className="bg-blue-500 text-white p-2 rounded-full"
+          onClick={handleNextClick}
+        >
+          Siguiente &rarr;
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default CartaMatch;
