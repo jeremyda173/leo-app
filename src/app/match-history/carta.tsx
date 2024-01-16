8cd193d339212de6c0330512b49592937f17f534
jeremyda173 <jh4017034@gmail.com>
2024-01-15 23:17:58 -0400

    feat: modify tailwindcss

--- src/app/match-history/carta.tsx
+++ src/app/match-history/carta.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
 
-import BrazilFlag from '../../../public/brazil-flag.png';
+import UruguayFlag from '../../../public/Uruguay.jpg';
 import ArgentinaFlag from '../../../public/bandera-argentina-re.png';
 
-function MatchHistory() {
+function GameA() {
   return (
     <div className="bg-gradient-to-r from-blue-800 via-blue-700 to-purple-800 text-white min-h-screen flex items-center ml-20">
       <div className="container mx-auto px-4 sm:px-96 relative">
@@ -14,21 +14,21 @@ function MatchHistory() {
 
           {/* Información del partido */}
           <div className="mb-4">
-            <p className="text-xl font-semibold mb-2 text-center">NOV 21</p>
-            <p className="text-sm text-center">Eliminatoria Sudamericana - Jornada 6</p>
+            <p className="text-xl font-semibold mb-2 text-center">NOV 16</p>
+            <p className="text-sm text-center">Eliminatoria Sudamericana - Jornada 5</p>
             <div className="grid grid-cols-2 gap-4 mt-4">
               <div className="text-center">
                 <div className="flex items-center justify-center space-x-2">
-                  <Image src={BrazilFlag} alt="Brazil Flag" width={50} height={50} />
-                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Brasil</h2>
+                  <Image src={UruguayFlag} alt="Uruguay Flag" width={50} height={50} />
+                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Uruguay</h2>
                 </div>
                 <div className="flex items-center justify-center space-x-2 mt-2 ml-36">
                   <div className="bg-purple-600 p-4 rounded-md w-max text-white">
-                    <p className="text-xl font-bold mb-2">0</p>
+                    <p className="text-xl font-bold mb-2">2</p>
                   </div>
                   <span className="text-xl font-bold text-purple-300">-</span>
                   <div className="bg-purple-600 p-4 rounded-md w-max text-white">
-                    <p className="text-xl font-bold mb-2">1</p>
+                    <p className="text-xl font-bold mb-2">0</p>
                   </div>
                 </div>
               </div>
@@ -50,4 +50,4 @@ function MatchHistory() {
   );
 }
 
-export default MatchHistory;
+export default GameA;
