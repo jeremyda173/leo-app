17bf57165fc88980bb8303ae1de7cc935326691b
jeremyda173 <jh4017034@gmail.com>
2024-01-16 15:28:28 -0400

    feat: modify route

--- src/app/match-history/layout.tsx
+++ src/app/match-history/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Menu from '@/app/menu';
+import Menu from '@/app/components/menu';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
