604f2f0b8c70316ce90f737dbb6ddee0d3516bfe
jeremyda173 <jh4017034@gmail.com>
2024-01-15 23:18:19 -0400

    feat: modify tailwindcss

--- src/app/match-history/carta2.tsx
+++ src/app/match-history/carta2.tsx
@@ -2,41 +2,52 @@ import React from 'react';
 import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
 
-import AlHilal from '../../../public/logo-al-hilal-2048.png';
-import InterMiami from '../../../public/LogoM.png';
+import BrazilFlag from '../../../public/brazil-flag.png';
+import ArgentinaFlag from '../../../public/bandera-argentina-re.png';
 
-
-function MatchHistory() {
+function GameB() {
   return (
-    <div className="bg-gradient-to-r from-blue-800 via-blue-700 to-purple-800 text-white min-h-screen flex items-center ml-12">
+    <div className="bg-gradient-to-r from-blue-800 via-blue-700 to-purple-800 text-white min-h-screen flex items-center ml-20">
       <div className="container mx-auto px-4 sm:px-96 relative">
-        <div className="bg-gray-800 text-whit p-4 rounded-md shadow-2xl w-96 mx-auto border border-gray-600 transform hover:scale-105 transition-transform">
+        <div className="bg-gray-800 text-white p-4 rounded-md shadow-2xl w-80 mx-auto border border-gray-600 transform hover:scale-105 transition-transform">
           <h1 className="text-3xl font-extrabold mb-4 text-center bg-purple-600 p-2 rounded">Match History</h1>
 
           {/* Información del partido */}
           <div className="mb-4">
-            <p className="text-xl font-semibold mb-2 text-center">JAN 29</p>
-            <p className="text-sm text-center">Amistoso</p>
-            <div className="flex items-center justify-center flex-col space-y-2 mt-4">
-              <div className="flex items-center space-x-2">
-                <Image src={AlHilal} alt="Brazil Flag" width={50} height={50} />
-                <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Al-Hilal</h2>
+            <p className="text-xl font-semibold mb-2 text-center">NOV 21</p>
+            <p className="text-sm text-center">Eliminatoria Sudamericana - Jornada 6</p>
+            <div className="grid grid-cols-2 gap-4 mt-4">
+              <div className="text-center">
+                <div className="flex items-center justify-center space-x-2">
+                  <Image src={BrazilFlag} alt="Brazil Flag" width={50} height={50} />
+                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Brasil</h2>
+                </div>
+                <div className="flex items-center justify-center space-x-2 mt-2 ml-36">
+                  <div className="bg-purple-600 p-4 rounded-md w-max text-white">
+                    <p className="text-xl font-bold mb-2">0</p>
+                  </div>
+                  <span className="text-xl font-bold text-purple-300">-</span>
+                  <div className="bg-purple-600 p-4 rounded-md w-max text-white">
+                    <p className="text-xl font-bold mb-2">1</p>
+                  </div>
+                </div>
               </div>
-              <span className="text-xl font-bold text-purple-300 pl-8">VS</span>
-              <div className="flex items-center space-x-2">
-                <Image src={InterMiami} alt="Argentina Flag" width={50} height={50} />
-                <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Inter-Miami</h2>
+              <div className="text-center">
+                <div className="flex items-center justify-center space-x-2">
+                  <Image src={ArgentinaFlag} alt="Argentina Flag" width={50} height={50} />
+                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Argentina</h2>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
         <div className="mt-4 bg-blue-500 text-white p-2 rounded-md text-center">
-          <p className="text-sm">Encuentro pendiente</p>
+          <p className="text-sm">Resultado Finalizado</p>
         </div>
       </div>
     </div>
   );
 }
 
-export default MatchHistory;
+export default GameB;
