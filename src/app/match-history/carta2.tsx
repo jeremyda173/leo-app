3c78753ff4ebd1b1da600dd5ee1ee883bfdbe125
jeremyda173 <jh4017034@gmail.com>
2024-01-24 14:08:48 -0400

    feat: modify file paths

--- src/app/match-history/carta2.tsx
+++ src/app/match-history/carta2.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
 
-import BrazilFlag from '../../../public/brazil-flag.png';
-import ArgentinaFlag from '../../../public/bandera-argentina-re.png';
+import Shield1 from '../../../public/Shield/LogoM.png';
+import Shield2 from '../../../public/Shield/fc-dallas.png';
 
 function GameB() {
   return (
@@ -14,13 +14,13 @@ function GameB() {
 
           {/* Información del partido */}
           <div className="mb-4">
-            <p className="text-xl font-semibold mb-2 text-center">NOV 21</p>
-            <p className="text-sm text-center">Eliminatoria Sudamericana - Jornada 6</p>
+            <p className="text-xl font-semibold mb-2 text-center">JAN 21</p>
+            <p className="text-sm text-center">Amistoso</p>
             <div className="grid grid-cols-2 gap-4 mt-4">
               <div className="text-center">
                 <div className="flex items-center justify-center space-x-2">
-                  <Image src={BrazilFlag} alt="Brazil Flag" width={50} height={50} />
-                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Brasil</h2>
+                  <Image src={Shield1} alt="Brazil Flag" width={50} height={50} />
+                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Inter Miami</h2>
                 </div>
                 <div className="flex items-center justify-center space-x-2 mt-2 ml-36">
                   <div className="bg-purple-600 p-4 rounded-md w-max text-white">
@@ -34,8 +34,8 @@ function GameB() {
               </div>
               <div className="text-center">
                 <div className="flex items-center justify-center space-x-2">
-                  <Image src={ArgentinaFlag} alt="Argentina Flag" width={50} height={50} />
-                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Argentina</h2>
+                  <Image src={Shield2} alt="Argentina Flag" width={50} height={50} />
+                  <h2 className="text-lg font-bold mb-2 mt-2 text-purple-300">Dallas FC</h2>
                 </div>
               </div>
             </div>
