bbe56c5130b9853875dca2faefb7f4519ba4ec5e
jeremyda173 <jh4017034@gmail.com>
2024-01-27 18:20:09 -0400

    fix: correcting error's in balon-oro

--- src/app/awards/page.tsx
+++ src/app/awards/page.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
-'use client';
-/* eslint-disable react/no-unescaped-entities */
-import React, { useState } from 'react';
+'use client'
+;import React, { useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import Individuals from '@/app/awards/individuals/page';
 import Teams from '@/app/awards/teams/page';
 
 const Awards = () => {
-  const [selectedOption, setSelectedOption] = useState('individuals');
+  const [selectedOption, setSelectedOption] = useState('false');
 
   const handleOptionChange = (option: React.SetStateAction<string>) => {
     setSelectedOption(option);
@@ -15,13 +14,13 @@ const Awards = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="space-x-8">
+      <div className="flex flex-col items-center mt-8">
         <button
           className={`${
             selectedOption === 'individuals'
               ? 'bg-blue-500 text-white'
               : 'bg-gray-300 text-gray-700'
-          } py-3 px-6 rounded-full focus:outline-none transition-colors duration-300`}
+          } py-2 px-4 rounded-md focus:outline-none transition-colors duration-300 mb-2`}
           onClick={() => handleOptionChange('individuals')}
         >
           Individuals
@@ -31,14 +30,14 @@ const Awards = () => {
             selectedOption === 'teams'
               ? 'bg-blue-500 text-white'
               : 'bg-gray-300 text-gray-700'
-          } py-3 px-6 rounded-full focus:outline-none transition-colors duration-300`}
+          } py-2 px-4 rounded-md focus:outline-none transition-colors duration-300`}
           onClick={() => handleOptionChange('teams')}
         >
           Teams
         </button>
       </div>
 
-      <div className="mt-8 w-full h-full p-6 border border-gray-300 rounded-md bg-white shadow-md">
+      <div className="mt-8 w-full h-3/4 p-6 border border-gray-300 rounded-md bg-white shadow-md">
         {selectedOption === 'individuals' && <Individuals />}
         {selectedOption === 'teams' && <Teams />}
       </div>
@@ -47,5 +46,3 @@ const Awards = () => {
 };
 
 export default Awards;
-
-
