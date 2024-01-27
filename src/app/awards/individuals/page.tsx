9243c323919bba3c6bede7e62353e8b87619f3dd
jeremyda173 <jh4017034@gmail.com>
2024-01-27 19:20:55 -0400

    fix: modify position text

--- src/app/awards/individuals/page.tsx
+++ src/app/awards/individuals/page.tsx
@@ -12,8 +12,8 @@ function PresentationIn() {
         <BallonDOr />
         <div className="p-2">
           <h2 className="text-lg font-semibold mb-1">Ballon d'Or</h2>
-          <p className="text-gray-700 text-sm">
-            Description or additional information about the award goes here.
+          <p className="text-gray-700 text-sm text-justify"> 
+          El "Balón de Oro" es un prestigioso premio de fútbol otorgado anualmente al mejor jugador del mundo, según la votación de periodistas deportivos de todo el mundo. El reconocimiento, creado por la revista France Football en 1956, destaca el rendimiento excepcional de futbolistas a lo largo del año calendario.
           </p>
         </div>
       </div>
@@ -22,8 +22,8 @@ function PresentationIn() {
         <Botaoro />
         <div className="p-2">
           <h2 className="text-lg font-semibold mb-1">Bota de Oro</h2>
-          <p className="text-gray-700 text-sm">
-            Description or additional information about the award goes here.
+          <p className="text-gray-700 text-sm text-justify">
+          La Bota de Oro es un premio que reconoce el talento y la capacidad goleadora de los futbolistas en las ligas europeas y se ha convertido en un símbolo de excelencia en el arte del gol.
           </p>
         </div>
       </div>
@@ -32,8 +32,8 @@ function PresentationIn() {
         <GoldenBoy />
         <div className="p-2">
           <h2 className="text-lg font-semibold mb-1">Golden Boy</h2>
-          <p className="text-gray-700 text-sm">
-            Description or additional information about the award goes here.
+          <p className="text-gray-700 text-sm text-justify">
+          El Premio Golden Boy es un trofeo anual que desde el año 2003 entrega el diario deportivo italiano Tuttosport al mejor futbolista del fútbol europeo menor de 21 años.
           </p>
         </div>
       </div>
@@ -41,9 +41,9 @@ function PresentationIn() {
       <div className="max-w-xs mx-2 mb-4 bg-white rounded-md overflow-hidden shadow-md">
         <TheBest />
         <div className="p-2">
-          <h2 className="text-lg font-semibold mb-1">The Best</h2>
-          <p className="text-gray-700 text-sm">
-            Description or additional information about the award goes here.
+          <h2 className="text-lg font-bold mb-1">The Best</h2>
+          <p className="text-gray-700 text-sm text-justify">
+          Los Premios The Best fueron creados por la FIFA en 2016 tras su 'divorcio' con France Football. Se premia al mejor jugador, a la mejor jugadora, al mejor entrenador masculino y al mejor entrenador o entrenadora femenina. 
           </p>
         </div>
       </div>
