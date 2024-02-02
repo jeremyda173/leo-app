1bf39935a2d82ba16a9dc40aaf6365d8f2e1496e
jeremyda173 <jh4017034@gmail.com>
2024-02-02 19:20:13 -0400

    feat: modify code

--- src/app/groups/barcelona/page.tsx
+++ src/app/groups/barcelona/page.tsx
@@ -1,10 +1,40 @@
-import react from 'react';
+'use client';
+import React, { useState } from "react";
+import Image from 'next/image';
+import BarcelonaImage from "@/../../public/Shield/LogoB.png";
 
 function Barcelona() {
-    return(
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
+
+  const toggleAdditionalInfo = () => {
+    setShowAdditionalInfo(!showAdditionalInfo);
+  };
+
+  return (
+    <div>
+      <h1>Barcelona FC</h1>
+      
+      <button onClick={toggleAdditionalInfo}>
+        {showAdditionalInfo ? "Ocultar información" : "Mostrar información"}
+      </button>
+
+      {showAdditionalInfo && (
         <div>
-            <h1>Psg fc</h1>
+          {/* Información adicional sobre el FC Barcelona */}
+          <p>
+            El FC Barcelona es un club de fútbol profesional con sede en
+            Barcelona, Cataluña, España. Fundado en 1899, el Barcelona es uno de los
+            clubes más exitosos y populares del mundo.
+          </p>
+
+          {/* Muestra la imagen del escudo del Barcelona */}
+          <div style={{ width: '100px', height: '100px' }}>
+            <Image src={BarcelonaImage} alt="Escudo del Barcelona" />
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
+
 export default Barcelona;
