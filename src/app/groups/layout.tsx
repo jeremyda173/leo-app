2a62304df934a010c9e6ed075d454b5cfe82e804
jeremyda173 <jh4017034@gmail.com>
2024-02-02 16:11:21 -0400

    chore: modify position

---
+++ src/app/groups/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Menu from '@/app/components/menu';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+<div className='flex flex-col md:overflow-hidden'>
+  <div className='w-full md:w-full'>
+    <Menu />
+  </div>
+  <div className='flex-grow p-6 md:overflow-y-auto md:p-4 md:flex md:flex-col'>
+    {children}
+  </div>
+</div>
+
+  );
+}
