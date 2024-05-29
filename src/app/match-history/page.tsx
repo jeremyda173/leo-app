7ad20c780dedf13afcf7d8e3e84a36aa5a1195aa
jeremyda173 <jh4017034@gmail.com>
2024-05-29 09:51:52 -0400

    style: adjust spacing

--- src/app/match-history/page.tsx
+++ src/app/match-history/page.tsx
@@ -22,34 +22,36 @@ function CartaMatch() {
   };
 
   const activeCard = cards[activeCardIndex];
-  
+
   return (
     <div>
-<div className="flex items-center justify-center">
-  <h1 className="text-2xl text-center font-bold mb-4 bg-gradient-to-r text-black p-4 rounded-md shadow-md max-w-md border-2 border-purple-700">
-    Games of Lionel Messi
-  </h1>
-</div>
-      <div className="flex space-x-4">
-        {activeCard === "MatchHistory" && <MatchHistory />}
-        {activeCard === "NextGameform" && <NextGameform />}
-        {activeCard === "MatchHistoryB" && <NextGameA />}
-        {activeCard === "MatchHistoryC" && <NextGameB />}
+      <div className="flex items-center justify-center">
+        <h1 className="text-2xl text-center font-bold mb-4 bg-gradient-to-r text-black p-4 rounded-md shadow-md max-w-md border-2 border-purple-700">
+          Games of Lionel Messi
+        </h1>
+      </div>
+      <div className="flex justify-center rounded-sm">
+        <div className="flex space-x-4">
+          {activeCard === "MatchHistory" && <MatchHistory />}
+          {activeCard === "NextGameform" && <NextGameform />}
+          {activeCard === "MatchHistoryB" && <NextGameA />}
+          {activeCard === "MatchHistoryC" && <NextGameB />}
+        </div>
       </div>
       <div className="flex justify-center mt-4">
-        <div className="flex space-x-48">
-        <button
-          className="bg-blue-500 text-white p-2 rounded-full mr-4 w-48"
-          onClick={handlePrevClick} disabled={activeCardIndex === 0}
-        >
-          &#8592;
-        </button>
-        <button
-          className="bg-blue-500 text-white p-2 rounded-full w-48"
-          onClick={handleNextClick} disabled={activeCardIndex === cards.length - 1}
-        >
-          &#8594;
-        </button>
+        <div>
+          <button
+            className="bg-blue-500 text-white p-2 rounded-full mr-4"
+            onClick={handlePrevClick} disabled={activeCardIndex === 0}
+          >
+            &#8592;
+          </button>
+          <button
+            className="bg-blue-500 text-white p-2 rounded-full"
+            onClick={handleNextClick} disabled={activeCardIndex === cards.length - 1}
+          >
+            &#8594;
+          </button>
         </div>
       </div>
     </div>
