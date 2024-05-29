e7c8ff37799be35616ce52abbed19d051fd7189d
jeremyda173 <jh4017034@gmail.com>
2024-05-29 13:19:51 -0400

    style: adjust spacing

--- src/app/groups/argentina/titles.tsx
+++ src/app/groups/argentina/titles.tsx
@@ -1,32 +1,68 @@
-import ChampionsImage from '@/../../public/Awards/Champions.png';
-import SuperCopaEuropa from '@/../../public/Awards/supercopa.png';
-import Liga from '@/../../public/Awards/LaLiga.png';
-import CopaRey from '@/../../public/Awards/Copa_del_Rey.png';
-import MundialC from '@/../../public/Awards/Copa_Mundial_de_Clubes.png';
-import SuperCopaSpain from '@/../../public/Awards/SupercopaEspaña.png';
+// import CopaAmerica from '@/../../public/Awards/copa-america.png';
+// import CopaConfederaciones from '@/../../public/Awards/Copa_Confederaciones.png';
+// import Mundial2022 from '@/../../public/Awards/Champions.png';
+// import Finallissima from '@/../../public/Awards/Artemio_franchi_trophy.png';
+// import Image, { StaticImageData } from 'next/image';
+
+// function ArgentinaTitles() {
+//   return (
+//     <div className="bg-gray-700 min-h-screen flex items-center justify-center w-full pt-8 pl-8 pr-8">
+//       <div className="container mx-auto text-white">
+//         <h1 className="text-4xl font-bold mb-8">Títulos de Lionel Messi con la Selección Argentina</h1>
+//         {/* Trofeos Internacionales */}
+//         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+//           {renderTrophyCard(CopaAmerica, "Copa América", 1)}
+//           {renderTrophyCard(CopaConfederaciones, "Copa Confederaciones", 1)}
+//           {renderTrophyCard(Mundial2022, "Mundial 2022", 1)}
+//           {renderTrophyCard(Finallissima, "Finallissima", 1)} 
+   
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function renderTrophyCard(
+//   imageSrc: string | StaticImageData,
+//   title: string,
+//   count: number
+// ) {
+//   const trophyCount = count.toString();
+
+//   return (
+//     <div className="bg-gray-800 rounded-md overflow-hidden shadow-lg p-4 md:p-6 mb-8 md:mb-0 border">
+//       <div className="bg-white p-4 md:p-6 text-center">
+//         <Image src={imageSrc} alt={title} className="w-24 h-24 md:w-32 md:h-32 object-contain mx-auto mb-4" />
+//         <p className="text-xl font-bold text-gray-800">{title}</p>
+//         <p className="text-base text-gray-600 mb-4">Cantidad: {trophyCount}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ArgentinaTitles;
+
 import Image from 'next/image';
+import CopaAmerica from '@/../../public/copa-america.png';
+import Mundial2022 from '@/../../public/Awards/world-cup.png';
+import Finallissima from '@/../../public/Awards/Artemio_franchi_trophy.png';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import { ReactElement, JSXElementConstructor, ReactNode, PromiseLikeOfReactNode } from 'react';
 
 function BarcelonaTitles() {
   return (
-    <div className="bg-gray-700 min-h-screen flex items-center justify-center w-full pt-8 pl-8 pr-8">
+    <div className="bg-gray-700 min-h-screen flex items-center justify-center w-full pt-8 pl-8 pr-8 text-center">
       <div className="container mx-auto text-white">
         <h1 className="text-4xl font-bold mb-8">Titles de Lionel Messi in FC Barcelona</h1>
         {/* Trofeos Europeos */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
-          {renderTrophyCard(ChampionsImage, "UEFA Champions League", 4)}
-          {renderTrophyCard(SuperCopaEuropa, "Super Copa de Europa", 3)}
-          {/* Agregar más trofeos europeos según sea necesario */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8 ml-64">
+          {renderTrophyCard(CopaAmerica, "Copa América", 1)}
         </div>
 
         {/* Trofeos Nacionales e Internacionales */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
-          {renderTrophyCard(CopaRey, "Copa del Rey", 8)}
-          {renderTrophyCard(MundialC, "FIFA Club World Cup", 3)}
-          {renderTrophyCard(SuperCopaSpain, "Supercopas de España", 7)}
-          {renderTrophyCard(Liga, "Ligas de España", 10)}
-          {/* Agregar más trofeos nacionales e internacionales según sea necesario */}
+         {renderTrophyCard(Mundial2022, "Mundial 2022", 1)}
+         {renderTrophyCard(Finallissima, "Finallissima", 1)} 
         </div>
       </div>
     </div>
@@ -54,3 +90,4 @@ function renderTrophyCard(
 
 
 export default BarcelonaTitles;
+
