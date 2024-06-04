99ce28d3738bd048889153c05d4dd96c6e8c0b0c
jeremyda173 <jh4017034@gmail.com>
2024-06-03 21:43:16 -0400

    chore: update the code

--- src/app/awards/layout.tsx
+++ src/app/awards/layout.tsx
@@ -6,7 +6,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
 <div className='flex flex-col md:overflow-hidden'>
   <div className='w-full md:w-full'>
-    <Menu />
+    <Menu darkMode={false} toggleDarkMode={function (): void {
+          throw new Error('Function not implemented.');
+        } } />
   </div>
   <div className='flex-grow p-6 md:overflow-y-auto md:p-4 md:flex md:flex-col'>
     {children}
