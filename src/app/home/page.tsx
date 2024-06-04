79d03e4ba63ea6e92e9fba0e18b0b5e2b1fd4afa
jeremyda173 <jh4017034@gmail.com>
2024-06-03 21:05:36 -0400

    chore: delete unnecessary code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -23,24 +23,6 @@ const Home = () => {
       {/* Contenido del Dashboard */}
       <div className="flex flex-col relative z-10 container mx-auto p-8">
         <h1 className="text-3xl text-center font-bold mb-6 text-blue-600">Welcome to the Leo Messi Page</h1>
-
-        {/* Dark Mode Toggle Button */}
-        <label className="relative inline-flex items-center cursor-pointer">
-          <input className="sr-only peer" value="" type="checkbox" />
-          <div
-            className="w-24 h-12 rounded-full ring-0 peer duration-500 outline-none bg-gray-200 overflow-hidden 
-           before:flex before:items-center before:justify-center after:flex after:items-center after:justify-center 
-           before:content-['☀️'] before:absolute before:h-10 before:w-10 before:top-1/2 before:bg-white 
-           before:rounded-full before:left-1 before:-translate-y-1/2 before:transition-all before:duration-700 
-           peer-checked:before:opacity-0 peer-checked:before:rotate-90 peer-checked:before:-translate-y-full 
-           shadow-lg shadow-gray-400 peer-checked:shadow-lg peer-checked:shadow-gray-700 peer-checked:bg-[#383838] 
-           after:content-['🌑'] after:absolute after:bg-[#1d1d1d] after:rounded-full after:top-[4px] after:right-1 
-           after:translate-y-full after:w-10 after:h-10 after:opacity-0 after:transition-all after:duration-700 
-           peer-checked:after:opacity-100 peer-checked:after:rotate-180 peer-checked:after:translate-y-0"
-          ></div>
-        </label>
-
-
         {/* Primer Bloque */}
         <div className='flex flex-col md:flex-row mb-12 mt-20'>
           {/* Imagen 1 */}
