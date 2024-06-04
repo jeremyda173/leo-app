e9e587fbdb1902b2da2e4ff33f769c6d3731c8e0
jeremyda173 <jh4017034@gmail.com>
2024-06-03 21:38:38 -0400

    feat: update the code

--- src/app/family/layout.tsx
+++ src/app/family/layout.tsx
@@ -1,24 +1,19 @@
-'use client';
-import React, { useState } from 'react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import Menu from '@/app/components/menu';
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={`flex flex-col md:overflow-hidden ${darkMode ? 'dark' : ''}`}>
-      <div className='w-full md:w-full'>
-        <Menu darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      </div>
-      <div className='flex-grow p-6 md:overflow-y-auto md:p-4 md:flex md:flex-col'>
-        {children}
-      </div>
-    </div>
+<div className='flex flex-col md:overflow-hidden'>
+  <div className='w-full md:w-full'>
+    <Menu darkMode={false} toggleDarkMode={function (): void {
+          throw new Error('Function not implemented.');
+        } } />
+  </div>
+  <div className='flex-grow p-6 md:overflow-y-auto md:p-4 md:flex md:flex-col'>
+    {children}
+  </div>
+</div>
+
   );
 }
-
-export default Layout;
