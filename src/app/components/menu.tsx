5fa6d25f736470a9d6428f9f748db245befa8fab
jeremyda173 <jh4017034@gmail.com>
2024-06-03 21:35:57 -0400

    feat: update component Menu.tsx

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -1,23 +1,18 @@
 'use client';
-import { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 import 'tailwindcss/tailwind.css';
 import MessiLogo from '@/../../public/messi-logo.png';
 import Image from "next/image";
+import DarkMode from "./dark-mode";
 
-function Menu() {
-    const [activeOption, setActiveOption] = useState(String);
-    const [darkMode, setDarkMode] = useState(false); // Estado para controlar el modo oscuro
+interface MenuProps {
+    darkMode: boolean;
+    toggleDarkMode: () => void;
+}
 
-    // Función para cambiar el modo entre claro y oscuro
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-    }
+const Menu: React.FC<MenuProps> = ({ darkMode, toggleDarkMode }) => {
+    const [activeOption, setActiveOption] = useState<string>('');
 
-    // Clases condicionales para los elementos del menú según el estado del modo oscuro
-    const menuItemClass = darkMode ? "text-white" : "text-black";
-    const menuBgClass = darkMode ? "bg-gray-800" : "bg-gray-200";
-    
     return (
         <>
             {/* Menú Horizontal */}
@@ -74,34 +69,7 @@ function Menu() {
                         {activeOption === 'family' && <span className="absolute left-0 bottom-0 w-full h-1 bg-white rounded"></span>}
                     </a>
                     {/* Toggle para el modo oscuro */}
-                    <label
-                        className="cursor-pointer relative h-[3em] w-[6em] rounded-full bg-[hsl(0,0%,7%)] shadow-[0px_2px_4px_0px_rgb(18,18,18,0.25),0px_4px_8px_0px_rgb(18,18,18,0.35)]"
-                    >
-                        <span
-                            className="absolute inset-[0.1em] rounded-full border-[1px] border-[hsl(0,0%,25%)]"
-                        ></span>
-                        <div
-                            className="absolute left-[0.5em] top-1/2 flex h-[2em] w-[2em] -translate-y-1/2 items-center justify-center rounded-full bg-white shadow-[inset_0px_2px_2px_0px_hsl(0,0%,85%)]"
-                        >
-                            <div
-                                className="h-[1.5em] w-[1.5em] rounded-full bg-[hsl(0,0%,7%)] shadow-[0px_2px_2px_0px_hsl(0,0%,85%)]"
-                            ></div>
-                        </div>
-                        <div
-                            className="absolute right-[0.5em] top-1/2 h-[0.25em] w-[1.5em] -translate-y-1/2 rounded-full bg-[hsl(0,0%,50%)] shadow-[inset_0px_2px_1px_0px_hsl(0,0%,40%)]"
-                        ></div>
-                        <input className="peer h-[1em] w-[1em] opacity-0" id="toggleDarkMode" name="" type="checkbox" checked={darkMode} />
-                        <span
-                            className="absolute left-[0.25em] top-1/2 flex h-[2.5em] w-[2.5em] -translate-y-1/2 items-center justify-center rounded-full bg-[rgb(26,26,26)] shadow-[inset_4px_4px_4px_0px_rgba(64,64,64,0.25),inset_-4px_-4px_4px_0px_rgba(16,16,16,0.5)] duration-300 peer-checked:left-[calc(100%-2.75em)]"
-                        >
-                            <span className="relative h-full w-full rounded-full">
-                                <span
-                                    className="absolute inset-[0.1em] rounded-full border-[1px] border-[hsl(0,0%,50%)]"
-                                ></span>
-                            </span>
-                        </span>
-                    </label>
-
+                    <DarkMode darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
                 </div>
             </div>
         </>
