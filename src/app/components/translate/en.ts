27b00f69c4c7b776e8e96943aa9c4a8c1eccadbc
jeremyda173 <jh4017034@gmail.com>
2024-06-05 13:15:19 -0400

    feat: add language files and modify Match History

---
+++ src/app/components/translate/en.ts
@@ -0,0 +1,26 @@
+const en = {
+    auth: {
+      team: 'Team',
+    },
+    stats: {
+      title: 'Stats',
+      goals: 'Goal(s)',
+      assists: 'Assist(s)',
+      distance: 'Distance',
+      minutes: 'Minutes',
+      shots: 'Shot(s)',
+      passes: 'Passes(c)',
+      position: 'Position',
+      performance: 'Performance',
+    },
+    teams: {
+      inter_miami: 'Inter Miami',
+      al_nassr: 'Al-Nassr',
+    },
+    dates: {
+      january_19: 'January 19',
+    },
+  };
+  
+  export default en;
+  
