904fa4ab2c2621c13c94299077932679bad252c9
jeremyda173 <jh4017034@gmail.com>
2024-06-07 15:24:23 -0400

    refactor: make reforms to cards

--- src/app/match-history/page.tsx
+++ src/app/match-history/page.tsx
@@ -1,51 +1,14 @@
 'use client';
-import React, { useState } from "react";
+import React from "react";
 import MatchHistory from "../match-history/carta";
 
 function CartaMatch() {
-  const cards = ["MatchHistory", "MatchHistoryB", "MatchHistoryC", "NextGameform"];
-  const [activeCardIndex, setActiveCardIndex] = useState(2);
-
-  const handleNextClick = () => {
-    if (activeCardIndex < cards.length - 1) {
-      setActiveCardIndex(activeCardIndex + 1);
-    }
-  };
-
-  const handlePrevClick = () => {
-    if (activeCardIndex > 0) {
-      setActiveCardIndex(activeCardIndex - 1);
-    }
-  };
-
-  const activeCard = cards[activeCardIndex];
 
   return (
     <div>
-      <div className="flex items-center">
-        <h1 className="text-2xl text-center font-bold mb-4 bg-gradient-to-r from-blue-400 via-blue-200 text-black p-4 rounded-md shadow-md max-w-md border-2">
-          Match schedule
-        </h1>
-      </div>
       <div className="flex justify-center rounded-sm">
-        <div className="flex space-x-4">
-          {activeCard === "MatchHistory" && <MatchHistory />}
-        </div>
-      </div>
-      <div className="flex justify-center mt-4">
-        <div>
-          <button
-            className="bg-blue-500 text-white p-2 rounded-full mr-4"
-            onClick={handlePrevClick} disabled={activeCardIndex === 0}
-          >
-            &#8592;
-          </button>
-          <button
-            className="bg-blue-500 text-white p-2 rounded-full"
-            onClick={handleNextClick} disabled={activeCardIndex === cards.length - 1}
-          >
-            &#8594;
-          </button>
+        <div className="flex space-x-4 p-4">
+         <MatchHistory />
         </div>
       </div>
     </div>
