3edd2f3a4ac8e9e5b1ad566c80d1565903229bf2
jeremyda173 <jh4017034@gmail.com>
2024-06-08 23:45:44 -0400

    feat: add new words traductions

--- src/app/components/translate/es.ts
+++ src/app/components/translate/es.ts
@@ -1,8 +1,11 @@
+import { Anek_Telugu } from "next/font/google";
+
 const es = {
   auth: {
     team: 'Equipo',
     friendly_match: 'Partido Amistoso',
     world_cup: 'Copa Mundial',
+    champions_league: 'Liga de Campeones',
     age: 'Edad',
   },
   stats: {
@@ -20,10 +23,28 @@ const es = {
   teams: {
     inter_miami: 'Inter Miami',
     al_nassr: 'Al-Nassr',
+    real_madrid: 'Real madrid',
+    fc_barcelona: 'FC Barcelona',
+    alemania: 'Alemania',
+    uruguay: 'Uruguay',
+    espanyol: 'Espanyol',
+    argentina: 'Argentina',
+    psg: 'PSG',
+    liverpool: 'Liverpool',
+    manchester_city: 'Manchester City',
+    manchester_united: 'Manchester United',
   },
   dates: {
     january_19: '19 de enero',
     september_10: '10 de septiembre',
+    may_10: '10 de mayo',
+    march_15: '15 de marzo',
+    april_3: '3 de abril',
+    september_12: '12 de septiembre',
+    june_20: '20 de junio',
+    february_5: '5 de febrero',
+    july_5: '5 de julio',
+    august_18: '18 de agosto',
   },
 };
 
