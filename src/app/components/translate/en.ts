6cb612a83b07e74b0ad62e4d1623df3e6c4ce593
jeremyda173 <jh4017034@gmail.com>
2024-06-08 23:45:42 -0400

    feat: add new words traductions

--- src/app/components/translate/en.ts
+++ src/app/components/translate/en.ts
@@ -1,3 +1,4 @@
+import Champions from "@/app/awards/teams/internacional/champions-league/page";
 import { Agent } from "http";
 
 const en = {
@@ -5,6 +6,7 @@ const en = {
       team: 'Team',
       friendly_match: 'Friendly Match',
       world_cup: 'World cup',
+      champions_league: 'Champions League',
       age: 'Age',
     },
     stats: {
@@ -22,10 +24,28 @@ const en = {
     teams: {
       inter_miami: 'Inter Miami',
       al_nassr: 'Al-Nassr',
+      real_madrid: 'Real madrid',
+      fc_barcelona: 'FC Barcelona',
+      alemania: 'Alemania',
+      uruguay: 'Uruguay',
+      espanyol: 'Espanyol',
+      argentina: 'Argentina',
+      psg: 'PSG',
+      liverpool: 'Liverpool',
+      manchester_city: 'Manchester City',
+      manchester_united: 'Manchester United',
     },
     dates: {
       january_19: 'January 19',
       september_10: 'September 10',
+      may_10: 'May 10',
+      march_15: 'May 15',
+      april_3: 'April 3',
+      september_12: 'September 12',
+      june_20: 'July 20',
+      february_5: 'February 5',
+      july_5: 'July 5',
+      august_18: 'August 18',
     },
     family: {
         family: 'Family',
