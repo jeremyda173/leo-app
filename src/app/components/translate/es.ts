8a9654cc0db8b178eec02815a50cff90b19bc51a
jeremyda173 <jh4017034@gmail.com>
2024-06-19 14:16:08 -0400

    refactor: improve code

--- src/app/components/translate/es.ts
+++ src/app/components/translate/es.ts
@@ -52,7 +52,7 @@ const es = {
     home: 'Inicio',
     match_history: 'Historial de Partidos',
     awards: 'Premios',
-    teams: 'Equipos',
+    groups: 'Equipos',
     family: 'Familia'
   },
   text: {
@@ -60,6 +60,14 @@ const es = {
     journey: "A los 13 años, el destino le tenía preparado un giro que cambiaría su vida para siempre. Messi dejó su Argentina natal y partió hacia Barcelona, España, para unirse a La Masia, la prestigiosa academia juvenil del FC Barcelona. La transición no fue fácil; extrañaba su tierra, su familia, pero el sueño de conquistar el fútbol europeo lo impulsaba a superar cualquier obstáculo.",
     masia: "En La Masia, Messi continuó su ascenso meteórico. Su habilidad con el balón, su visión de juego y su velocidad asombraban a sus compañeros y entrenadores por igual. El joven argentino se convirtió en una joya preciada, y su nombre empezó a resonar en todo el mundo futbolístico. A los 16 años, llegó el momento crucial: Lionel Messi debutó con el primer equipo del FC Barcelona en un partido amistoso.",
     international: "Pero Messi también llevó su genialidad al escenario internacional. Con la selección argentina, vivió momentos épicos y desafíos. La Copa del Mundo de la FIFA se convirtió en una búsqueda incansable para Messi, un deseo ardiente de llevar a su país a la gloria máxima. Cada torneo era una nueva oportunidad para que 'La Pulga' demostrara su valía en el escenario mundial."
+  },
+  awards: {
+    golden_ball: " El Balón de Oro es un prestigioso premio de fútbol otorgado anualmente al mejor jugador del mundo, según la votación de periodistas deportivos de todo el mundo. El reconocimiento, creado por la revista France Football en 1956, destaca el rendimiento excepcional de futbolistas a lo largo del año calendario.",
+    golden_boot: " La Bota de Oro es un premio que reconoce el talento y la capacidad goleadora de los futbolistas en las ligas europeas y se ha convertido en un símbolo de excelencia en el arte del gol.",
+    golden_boy: " El Premio Golden Boy es un trofeo anual que desde el año 2003 entrega el diario deportivo italiano Tuttosport al mejor futbolista del fútbol europeo menor de 21 años.",
+    the_best: " Los Premios The Best fueron creados por la FIFA en 2016 tras su 'divorcio' con France Football. Se premia al mejor jugador, a la mejor jugadora, al mejor entrenador masculino y al mejor entrenador o entrenadora femenina.",
+    laureus: " Los Premios Laureus World Sports, también conocidos como los Oscar del deporte, son una serie de galardones anuales que honran a los deportistas más destacados del mundo. Fundados en 1999 por la Laureus Sport for Good Foundation, estos premios reconocen logros deportivos excepcionales en varias categorías.",
+    pichichi: " El Trofeo Pichichi es un galardón otorgado anualmente por el diario deportivo español Marca al máximo goleador de la Primera División de España. Nombrado en honor al legendario delantero del Athletic Club, Rafael Pichichi Moreno, este premio reconoce la destreza y el talento de los goleadores más destacados de La Liga."
   },
 };
 
