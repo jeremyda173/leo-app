110f2972c02f6ad3d50df827e1c4da6c5488c75f
jeremyda173 <jh4017034@gmail.com>
2024-06-22 13:50:15 -0400

    chore: update code

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -4,7 +4,6 @@ interface ModalProps {
   onClose: () => void;
   children: ReactNode;
 }
-
 const Modal: React.FC<ModalProps> = ({ onClose, children }) => (
   <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
     <div className="bg-white p-8 rounded-lg relative">
