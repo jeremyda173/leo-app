ab27664c81c9851d95890672c1fb389120231079
jeremyda173 <jh4017034@gmail.com>
2024-06-27 19:56:36 -0400

    refactor: improve code

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,23 +2,40 @@ import { FaInstagram, FaFacebook, FaGoogle } from 'react-icons/fa';
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 
-function Footer () {
+function Footer() {
     return (
-        <>
-         {/* Pie de página con enlaces a redes sociales, íconos y derechos reservados */}
-      <footer className="bg-gray-800 text-white p-4 text-center flex items-center justify-center space-x-4">
-        <a href="https://www.instagram.com/leomessi" target="_blank" rel="noopener noreferrer">
-          <FaInstagram size={30} color="#fff" />
-        </a>
-        <a href="https://www.facebook.com/LeoMessi" target="_blank" rel="noopener noreferrer">
-          <FaFacebook size={30} color="#fff" />
-        </a>
-        <a href="https://messi.com/fundacion-leo-messi/" target="_blank" rel="noopener noreferrer">
-          <FaGoogle size={30} color="#fff" />
-        </a>
-        <p className="ml-2">&copy; 2023 Messi Fan Club. Todos los derechos reservados.</p>
-      </footer>
-        </>
+        <footer className="bg-gradient-to-r from-gray-900 to-gray-800 text-white py-6 px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-between">
+            <div className="flex items-center space-x-4 mb-4 sm:mb-0">
+                <a
+                    href="https://www.instagram.com/leomessi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-300 hover:text-gray-400 transition-colors flex items-center"
+                >
+                    <FaInstagram className="mr-2" size={24} />
+                    Instagram
+                </a>
+                <a
+                    href="https://www.facebook.com/LeoMessi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-300 hover:text-gray-400 transition-colors flex items-center"
+                >
+                    <FaFacebook className="mr-2" size={24} />
+                    Facebook
+                </a>
+                <a
+                    href="https://messi.com/fundacion-leo-messi/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-300 hover:text-gray-400 transition-colors flex items-center"
+                >
+                    <FaGoogle className="mr-2" size={24} />
+                    Sitio Oficial
+                </a>
+            </div>
+            <p className="text-center sm:text-right text-sm">&copy; 2023 Messi Fan Club. Todos los derechos reservados.</p>
+        </footer>
     );
 }
 
