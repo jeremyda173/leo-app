c0d3157e96c3cf5f185e78a95eced206b7903bda
jeremyda173 <jh4017034@gmail.com>
2024-06-28 22:12:27 -0400

    refactor: improve code

--- src/app/family/layout.tsx
+++ src/app/family/layout.tsx
@@ -1,18 +1,16 @@
-'use client';
-import React from 'react';
 import Menu from '@/app/components/menu';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+
   return (
-    <div className="flex flex-col md:overflow-hidden">
-      <div className="w-full md:w-full">
+    <div className='flex flex-col md:overflow-hidden'>
+      <div className='w-full md:w-full'>
         <Menu />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-4 md:flex md:flex-col">
+      <div className='flex-grow p-6 md:overflow-y-auto md:p-0 md:flex md:flex-col'>
         {children}
       </div>
     </div>
   );
 }
-
 export default Layout;
