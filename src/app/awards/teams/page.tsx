4981747c5babc3ace277a946250462abe0db52f6
jeremyda173 <jh4017034@gmail.com>
2024-06-28 22:12:18 -0400

    refactor: improve code

--- src/app/awards/teams/page.tsx
+++ src/app/awards/teams/page.tsx
@@ -9,55 +9,55 @@ const trophiesData = [
   {
     team: "FC Barcelona",
     competition: "La Liga",
-    years: "10 veces (2004–05, 2005–06, 2008–09, 2009–10, 2010–11, 2012–13, 2014–15, 2015–16, 2017–18, 2018–19)",
+    years: "10 times (2004–05, 2005–06, 2008–09, 2009–10, 2010–11, 2012–13, 2014–15, 2015–16, 2017–18, 2018–19)",
     logo: BarcelonaLogo,
   },
   {
     team: "Paris Saint-Germain (PSG)",
     competition: "Ligue 1",
-    years: "1 vez (2021–22)",
+    years: "1 time (2021–22)",
     logo: PSGLogo,
   },
   {
     team: "Paris Saint-Germain (PSG)",
-    competition: "Supercopa de Francia",
-    years: "2 veces (2021, 2022)",
+    competition: "French Super Cup",
+    years: "2 times (2021, 2022)",
     logo: PSGLogo,
   },
   {
     team: "FC Barcelona",
-    competition: "Champions League",
-    years: "4 veces (2005–06, 2008–09, 2010–11, 2014–15)",
+    competition: "UEFA Champions League",
+    years: "4 times (2005–06, 2008–09, 2010–11, 2014–15)",
     logo: BarcelonaLogo,
   },
   {
     team: "FC Barcelona",
-    competition: "Supercopa de España",
-    years: "8 veces (2005, 2006, 2009, 2010, 2011, 2013, 2016, 2018)",
+    competition: "Spanish Super Cup",
+    years: "8 times (2005, 2006, 2009, 2010, 2011, 2013, 2016, 2018)",
     logo: BarcelonaLogo,
   },
   {
     team: "FC Barcelona",
-    competition: "Supercopa de Europa",
-    years: "3 veces (2009, 2011, 2015)",
+    competition: "UEFA Super Cup",
+    years: "3 times (2009, 2011, 2015)",
     logo: BarcelonaLogo,
   },
   {
     team: "FC Barcelona",
-    competition: "Mundial de Clubes",
-    years: "3 veces (2009, 2011, 2015)",
+    competition: "FIFA Club World Cup",
+    years: "3 times (2009, 2011, 2015)",
     logo: BarcelonaLogo,
   },
   {
     team: "FC Barcelona",
     competition: "Copa del Rey",
-    years: "7 veces (2008–09, 2011–12, 2014–15, 2015–16, 2016–17, 2017–18, 2020–21)",
+    years: "7 times (2008–09, 2011–12, 2014–15, 2015–16, 2016–17, 2017–18, 2020–21)",
     logo: BarcelonaLogo,
   },
   {
     team: "Inter Miami",
     competition: "MLS Cup",
-    years: "1 vez (2023-2024)",
+    years: "1 time (2023-2024)",
     logo: InterMiamiLogo,
   },
 ];
@@ -65,13 +65,13 @@ const trophiesData = [
 const TrophiesTable: React.FC = () => {
   return (
     <div className="p-6 bg-gray-100 rounded-lg shadow-lg max-w-full mx-auto mt-12 overflow-x-auto">
-      <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Títulos de Lionel Messi en Clubes</h1>
+      <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Lionel Messi&apos;s Club Titles</h1>
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
           <tr>
-            <th scope="col" className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Equipo</th>
-            <th scope="col" className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Competición</th>
-            <th scope="col" className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Años</th>
+            <th scope="col" className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Team</th>
+            <th scope="col" className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Competition</th>
+            <th scope="col" className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">Years</th>
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
