60ddb14d6576503e9afc6e4b34f098e38ca4afa4
jeremyda173 <jh4017034@gmail.com>
2024-06-28 20:32:57 -0400

    refactor: improve code

--- src/app/groups/layout.tsx
+++ src/app/groups/layout.tsx
@@ -3,16 +3,11 @@ import React, { useState } from 'react';
 import Menu from '@/app/components/menu';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
 
   return (
-    <div className={`flex flex-col md:overflow-hidden ${darkMode ? 'dark' : ''}`}>
+    <div className='flex flex-col md:overflow-hidden'>
       <div className='w-full md:w-full'>
-        <Menu darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <Menu />
       </div>
       <div className='flex-grow p-4 md:overflow-y-auto md:p-0 md:flex md:flex-col'>
         {children}
