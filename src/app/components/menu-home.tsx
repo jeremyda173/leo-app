34d07aa4cfe04f0d9876a6c4c4fcac82f28cc3a0
jeremyda173 <jh4017034@gmail.com>
2024-07-06 21:25:39 -0400

    chore: change the code

--- src/app/components/menu-home.tsx
+++ src/app/components/menu-home.tsx
@@ -20,19 +20,19 @@ const Menu: React.FC<MenuProps> = ({ darkMode, toggleDarkMode }) => {
             
             {/* Contenedor del menú */}
             <div className="absolute top-0 left-0 right-0 z-10 bg-gradient-to-r from-purple-600 to-blue-500 bg-opacity-75 py-4 shadow-lg">
-                <div className="container mx-auto flex items-center justify-between">
+                <div className="container mx-auto flex flex-col items-center">
                     {/* Logo de Messi */}
                     <div className="flex items-center">
                         <Image src={MessiLogo} alt="Messi Logo" width={50} height={50} className="mr-4"/>
                         <span className="text-white text-2xl font-bold">Messi</span>
                     </div>
                     {/* Opciones del Menú */}
-                    <div className="flex items-center space-x-8">
+                    <div className="flex flex-col items-center mt-4 space-y-4">
                         {["home", "match-history", "awards", "teams", "family"].map(option => (
                             <a
                                 key={option}
                                 href={`/${option}`}
-                                className={`relative text-white font-semibold transition-all duration-300 ${
+                                className={`text-white font-semibold transition-all duration-300 ${
                                     activeOption === option ? 'text-gray-200' : ''
                                 }`}
                                 onMouseEnter={() => setActiveOption(option)}
@@ -40,7 +40,7 @@ const Menu: React.FC<MenuProps> = ({ darkMode, toggleDarkMode }) => {
                             >
                                 {option.charAt(0).toUpperCase() + option.slice(1).replace('-', ' ')}
                                 {activeOption === option && (
-                                    <span className="absolute left-0 bottom-0 w-full h-1 bg-white rounded"></span>
+                                    <span className="block w-full h-1 bg-white rounded"></span>
                                 )}
                             </a>
                         ))}
