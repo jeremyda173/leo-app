cde85f9e0e48c3244cfd80d4c022db6f662158ae
jeremyda173 <jh4017034@gmail.com>
2024-07-24 11:55:36 -0400

    refactor: improve code

--- src/app/components/menu-home.tsx
+++ src/app/components/menu-home.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import 'tailwindcss/tailwind.css';
 import MessiLogo from '@/../../public/messi-logo.png';
 import Portada from '@/../../public/Portada2.jpg';
 import Image from "next/image";
+import { useRouter } from 'next/router';
 
 interface MenuProps {
     darkMode: boolean;
@@ -12,18 +13,25 @@ interface MenuProps {
 
 const Menu: React.FC<MenuProps> = ({ darkMode, toggleDarkMode }) => {
     const [activeOption, setActiveOption] = useState<string>('');
+    const router = useRouter();
+
+    useEffect(() => {
+        // Get the current path and set the active option accordingly
+        const path = router.pathname.split('/')[1];
+        setActiveOption(path);
+    }, [router.pathname]);
 
     return (
         <div className="relative">
             {/* Imagen de fondo */}
             <Image src={Portada} layout="fill" objectFit="cover" alt="Portada" className="z-0" />
-            
+
             {/* Contenedor del menú */}
             <div className="absolute top-0 left-0 right-0 z-10 bg-gradient-to-r from-purple-600 to-blue-500 bg-opacity-75 py-4 shadow-lg">
                 <div className="container mx-auto flex flex-col items-center">
                     {/* Logo de Messi */}
                     <div className="flex items-center">
-                        <Image src={MessiLogo} alt="Messi Logo" width={50} height={50} className="mr-4"/>
+                        <Image src={MessiLogo} alt="Messi Logo" width={50} height={50} className="mr-4" />
                         <span className="text-white text-2xl font-bold">Messi</span>
                     </div>
                     {/* Opciones del Menú */}
@@ -36,7 +44,7 @@ const Menu: React.FC<MenuProps> = ({ darkMode, toggleDarkMode }) => {
                                     activeOption === option ? 'text-gray-200' : ''
                                 }`}
                                 onMouseEnter={() => setActiveOption(option)}
-                                onMouseLeave={() => setActiveOption('')}
+                                onMouseLeave={() => setActiveOption(router.pathname.split('/')[1])}
                             >
                                 {option.charAt(0).toUpperCase() + option.slice(1).replace('-', ' ')}
                                 {activeOption === option && (
