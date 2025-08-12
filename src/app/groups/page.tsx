b5257694c9ad9d85ae8098883f1a74edc74bcd01
jeremyda173 <jeremygithud173@gmail.com>
2025-08-11 22:13:13 -0400

    refactor: clean up whitespace in page component

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -2,12 +2,9 @@ import 'tailwindcss/tailwind.css';
 import React from 'react';
 import Groups from '@/app/groups/total-groups';
 
-
 function Page() {
-
   return (
         <Groups />
   );
 }
-
 export default Page;
