c7e34408491ebefeb370c7717874029d2a3fb8da
jeremyda173 <jeremygithud173@gmail.com>
2025-09-22 09:01:50 -0400

    refactor: remove unused comment in Home component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
 import React, { useState } from 'react';
-import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
-import Portada from '@/../../public/Portada.jpg';
-import Masia from '@/../../public/masia.jpg';
-import MessiFr from '@/../../public/MessiFr.jpg';
 import Footer from '@/app/components/footer';
-import Menu from '@/app/components/menu-home';
 import Welcome from '@/app/components/welcome';
 
 const Home = () => {
@@ -19,7 +14,6 @@ const Home = () => {
 
   return (
     <div className={`${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-800'} min-h-screen flex flex-col relative overflow-hidden transition-colors duration-500`}>
-      {/* Contenido del Dashboard */}
       <div className="flex flex-col relative z-10 container mx-auto">
         <div>
           <Welcome />
